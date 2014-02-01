9e6f860ae24d710b090482d6ddd04a76caf853b2
Tom Li <biergaizi2009@gmail.com>
2014-02-02 07:08:20 +0800

    重命名为 sinaweibopy3，更新信息

--- setup.py
+++ setup.py
@@ -1,30 +1,25 @@
 from distutils.core import setup
-import sys
-
-import weibo
+import weibo3
 
 kw = dict(
-    name = 'sinaweibopy',
-    version = weibo.__version__,
+    name = 'sinaweibopy3',
+    version = weibo3.__version__,
     description = 'Sina Weibo OAuth 2 API Python SDK',
     long_description = open('README', 'r').read(),
     author = 'Michael Liao',
     author_email = 'askxuefeng@gmail.com',
-    url = 'https://github.com/michaelliao/sinaweibopy',
-    download_url = 'https://github.com/michaelliao/sinaweibopy',
-    py_modules = ['weibo'],
+    url = 'https://github.com/WeCase/sinaweibopy',
+    download_url = 'https://github.com/WeCase/sinaweibopy',
+    py_modules = ['weibo3'],
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python 3',
         'Topic :: Internet',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ])
-
-if sys.version_info[1]==5:
-    kw['install_requires'] = ['simplejson']
 
 setup(**kw)
